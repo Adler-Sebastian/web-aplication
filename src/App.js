77a49b551fe6d2941ed73e54caf5242b59969e3f
Adler Sebastian <adlersebas91@gmail.com>
2024-09-22 16:50:17 -0300

    feat: Cards a mais

--- src/App.js
+++ src/App.js
@@ -16,8 +16,25 @@ function App() {
         <section className='box-req'>
           <h2>Ferramentas</h2>
           <p>Veja ferramentas abaixo que serão úteis nessa sua jornada </p>
+          <div className='box-cards'>
+            <div className='card'>
+              <h3>Dicas</h3>
+              <p>Encontre aqui dicas para deixar seu dia leve</p>
+            </div>
+            <div className='card'>
+              <h3>Livros</h3>
+              <p>Encontre aqui livros de auxílio</p>
+            </div>
+            <div className='card'>
+              <h3>Feedback</h3>
+              <p>Deixe aqui seu feedback do que podemos melhorar</p>
+            </div>
+          </div>
         </section>
       </main>
+      <footer>
+        <p className='autor'>API Saúde Mental - Sebastian</p>
+      </footer>
     </div>
   );
 }
