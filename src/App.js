af4b5671e9d8df516b386a260192405f03c9152c
Adler Sebastian <adlersebas91@gmail.com>
2024-09-22 16:41:05 -0300

    feat: Colocando ferramentas

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
         <section className='box-titulo'>
           <h1>Saúde Mental</h1>
           <p>Landing page de Aplicações sobre Saúde Mental</p>
+          <a href='reactjs.org' target='_blank'>Inicio</a>
+        </section>
+        <section className='box-req'>
+          <h2>Ferramentas</h2>
+          <p>Veja ferramentas abaixo que serão úteis nessa sua jornada </p>
         </section>
       </main>
     </div>
